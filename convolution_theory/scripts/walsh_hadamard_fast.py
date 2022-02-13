552014bb3ddabaff896a768053c18c597d15ebd6
mstrielnikov <37501284+mstrielnikov@users.noreply.github.com>
2022-02-14 00:09:28 +0200

    Added fast Walsh Hadamard implementation script

---
+++ convolution_theory/scripts/walsh_hadamard_fast.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+def WalshHadamard(vector) -> None:
+    vectorLen = len(vector)
+    transform = []
+    h = 1
+    while h < vectorLen:
+        for i in range(0, vectorLen, h * 2):
+            for j in range(i, i + h):
+                x = vector[j]
+                y = vector[j + h]
+                transform[j] = x + y
+                transform[j + h] = x - y
+        h *= 2
+    return transform
+  
+if __name__ == "__main__" :
+    print(WalshHadamard(sys.argv[1]))
+  
